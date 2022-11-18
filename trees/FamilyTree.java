ec9732a581883ee3185d7c538cecdfd4d82bb604
CharlesM <charlesvincent.manaois@sjsu.edu>
2022-11-18 09:18:36 -0800

    updated TreeNode class

--- trees/FamilyTree.java
+++ trees/FamilyTree.java
@@ -58,7 +58,11 @@ public class FamilyTree {
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
-            
+            TreeNode curr = this.parent;
+            while(curr != null) {
+                ancestors.add(curr);
+                curr = curr.parent;
+            }
             return ancestors;
         }
 
